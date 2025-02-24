f7f94c0bffc6a4f4cea9e344bb34e018d38d00fb
a456f <56493335+a456f@users.noreply.github.com>
2025-02-24 12:57:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-
 // Importar dependencias
 const webpush = require("web-push");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path"); // Importa el módulo path para trabajar con rutas de archivos
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Claves VAPID generadas
+// Claves VAPID generadas (reemplaza por las claves reales)
 const publicVapidKey = "BAvJThBSIt_13X7E498WBbaKLfCGa_nu9XOMqJO7jfnChUpY0JqfyCgxbsMDLaOHfqOP7WB-PsHSgXAfuxRcwCQ";
 const privateVapidKey = "s3MUYjz3Eyg6AzsaZ4FRBM2Wtk_t-K5sK-JQ3hfQg6g";
 
@@ -36,5 +36,8 @@ app.post("/sendNotification", (req, res) => {
     res.json({ message: "Notificación enviada con éxito" });
 });
 
+// Servir archivos estáticos desde la carpeta actual
+app.use(express.static(__dirname));
 
-app.listen(5001, () => console.log("Servidor corriendo en el puerto 5001"));
+// Configurar servidor para escuchar en todas las interfaces y en el puerto 5001
+app.listen(5001, '0.0.0.0', () => console.log("Servidor corriendo en el puerto 5001"));
